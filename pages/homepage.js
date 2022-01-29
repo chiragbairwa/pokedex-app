fe61fa0afdb3f17cd577534f13a51d1c0923732c
chirag <chirag.dev18@gmail.com>
2022-01-29 20:27:25 +0530

    First Commit

---
+++ pages/homepage.js
@@ -0,0 +1,11 @@
+import styles from '../styles/homepage.module.css'
+
+function Homepage() {
+  return (
+    <div className={styles.Homepage}>
+      <div className={styles.PhotoBackground}></div>
+    </div>
+  )
+}
+
+export default Homepage
