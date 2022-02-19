07078dfceeaa4288071c63bb0284fbf759f1d6e9
chirag <chirag.dev18@gmail.com>
2022-02-19 21:32:51 +0530

    Search Bar suggestions BUG

--- pages/header.js
+++ pages/header.js
@@ -1,8 +1,31 @@
+import ReactSearchBox from 'react-search-box'
+import pokedex from '../public/pokedex'
+
 const Header = () => {
+  let newdata = []
+  let temp = {}
+
+  for (let n = 0; n < 3; n++) {
+    let jsondata = pokedex[n].name.english
+    console.log(jsondata)
+    Object.assign(newdata, jsondata)
+    // Object.assign(temp, { key: pokedex[n].name.english })
+    // Object.assign(temp, { value: pokedex[n].name.english })
+    // newdata.push(temp)
+    // console.log(newdata)
+    // console.log(newdata[n])
+  }
+
   return (
     <div className="header">
       <h1>Pokedex</h1>
-      <input type="text"></input>
+      <div className="search-bar">
+        <ReactSearchBox
+          placeholder="Search Pokemon Name"
+          data={[newdata]}
+          onChange={(value) => console.log(newdata)}
+        ></ReactSearchBox>
+      </div>
     </div>
   )
 }
--- pages/homepage.js
+++ pages/homepage.js
@@ -1,57 +1,94 @@
-import { useState } from 'react'
 import Header from './header'
-import InfoType from './infotype'
+import { useState } from 'react'
+import pokedex from '../public/pokedex'
 
 export default function Homepage() {
   const [number, setNumber] = useState(1)
 
+  const imageSource =
+    'https://github.com/fanzeyi/pokemon.json/raw/master/images/'
+  let num = ''
+  function numSort() {
+    if (number < 10) {
+      num = '00' + number
+    } else if (number < 100) {
+      num = '0' + number
+    } else {
+      num = number
+    }
+
+    return num
+  }
   return (
     <div className="homepage">
       <Header></Header>
-      <div className="photo-name-container">
-        {/* Picture */}
-        <img src="/vercel.svg" alt="Pokemon Picture" />
 
-        {/* Number & Name */}
+      {/* Picture */}
+      <img
+        src={imageSource + numSort() + '.png'}
+        alt={`${pokedex[number - 1].id}`}
+      />
+
+      {/* Number & Name & Slider*/}
+      <div className="bio-slider-container">
         <div className="poke-bio">
-          <h2>#004</h2>
-          <h3>Bulbasaur</h3>
+          <h2>#{numSort()}</h2>
+          <h3>{pokedex[number - 1].name.english}</h3>
         </div>
-      </div>
 
-      {/* Slider */}
-      <div className="slider">
-        <button
-          onClick={() => setNumber(number - 2)}
-          className="slider-btn-low"
-        >
-          {number - 2}
-        </button>
-        <button
-          onClick={() => setNumber(number - 1)}
-          className="slider-btn-mid"
-        >
-          {number - 1}
-        </button>
-        <button onClick={() => setNumber(number)}>{number}</button>
-        <button
-          onClick={() => setNumber(number + 1)}
-          className="slider-btn-mid"
-        >
-          {number + 1}
-        </button>
-        <button
-          onClick={() => setNumber(number + 2)}
-          className="slider-btn-low"
-        >
-          {number + 2}
-        </button>
+        {/* Slider */}
+        <div className="slider">
+          <button
+            onClick={() => setNumber(number - 2)}
+            className="slider-btn-low"
+          >
+            {number - 2}
+          </button>
+          <button
+            onClick={() => setNumber(number - 1)}
+            className="slider-btn-mid"
+          >
+            {number - 1}
+          </button>
+          <button onClick={() => setNumber(number)}>{number}</button>
+          <button
+            onClick={() => setNumber(number + 1)}
+            className="slider-btn-mid"
+          >
+            {number + 1}
+          </button>
+          <button
+            onClick={() => setNumber(number + 2)}
+            className="slider-btn-low"
+          >
+            {number + 2}
+          </button>
+        </div>
       </div>
 
       {/* Information */}
       <div className="poke-biodata">
         <div className="poke-info"></div>
-        <InfoType></InfoType>
+        <div className="poke-type">
+          <div>
+            <p>Hp : {pokedex[number - 1].base.HP}</p>
+          </div>
+          <div>
+            <p>Attack: {pokedex[number - 1].base.Attack}</p>
+          </div>
+          <div>
+            <p>Defence: {pokedex[number - 1].base.Defense}</p>
+          </div>
+          <div>
+            <p>hello: {pokedex[number - 1].base['Sp. Attack']}</p>
+          </div>
+          <div>
+            <p>Sp. Defense: {`${pokedex[number - 1].base['Sp. Defense']}`}</p>
+          </div>
+          <div>
+            <p>Speed: {pokedex[number - 1].base.Speed}</p>
+          </div>
+        </div>
       </div>
     </div>
   )
